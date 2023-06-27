e49153a3c885f0f1d92ed2cb737fd987813d8b65
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-06-27 10:43:34 +0900

    refactor: 빌더 패턴으로 Drink 구현
    #27

--- kiosk-be/src/main/java/team04/kioskbe/domain/Drink.java
+++ kiosk-be/src/main/java/team04/kioskbe/domain/Drink.java
@@ -1,15 +1,28 @@
 package team04.kioskbe.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Drink {
 
-    private Long id;
-    private String name;
-    private String img;
-    private int price;
-    private Category category;
-    private List<Option> options;
+    private final Long id;
+    private final String name;
+    private final String img;
+    private final int price;
+    private final Category category;
+    private final List<Option> options = new ArrayList<>();
+
+    private Drink(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.img = builder.img;
+        this.price = builder.price;
+        this.category = builder.category;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 
     public Long getId() {
         return id;
@@ -33,5 +46,42 @@ public class Drink {
 
     public List<Option> getOptions() {
         return options;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String img;
+        private int price;
+        private Category category;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder img(String img) {
+            this.img = img;
+            return this;
+        }
+
+        public Builder price(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder category(Category category) {
+            this.category = category;
+            return this;
+        }
+
+        public Drink build() {
+            return new Drink(this);
+        }
     }
 }
