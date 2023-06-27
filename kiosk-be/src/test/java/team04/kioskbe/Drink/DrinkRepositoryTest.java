5654c006f2c259caef9f677c871f86725b5c47c3
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-06-27 21:37:32 +0900

    test: 음료 목록 조회 메소드 테스트 코드 수정
    #27
    - 주문 Dummy 데이터 생성
    - 총 판매 수량 검증 추가

--- kiosk-be/src/test/java/team04/kioskbe/Drink/DrinkRepositoryTest.java
+++ kiosk-be/src/test/java/team04/kioskbe/Drink/DrinkRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import team04.kioskbe.domain.Category;
 import team04.kioskbe.domain.Drink;
 import team04.kioskbe.domain.Option;
@@ -14,14 +17,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@Transactional
 public class DrinkRepositoryTest {
     @Autowired
     DrinkRepository drinkRepository;
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Test
-    @DisplayName("카테고리로 음료 목록 조회시 해당하는 음료 목록을 반환한다.")
+    @DisplayName("카테고리로 음료 목록 조회시 해당하는 음료 목록을 당일 판매수량 기준으로 정렬하여 반환한다.")
     public void findByCategoryTest() {
         //given
+        String orderInfoInsert = "INSERT INTO order_info(payment_type, total_amount, received_amount) VALUES ('Cash', 8000, 8000), ('Cash', 4000, 4000), ('Card', 8000, 8000);";
+        String orderDrinkInsert = "INSERT INTO order_drink(order_id, drink_id, quantity, order_price) VALUES (1,4, 2, 8000), (1,4, 1, 4000), (1,1, 4, 8000);";
+        namedParameterJdbcTemplate.update(orderInfoInsert, new MapSqlParameterSource());
+        namedParameterJdbcTemplate.update(orderDrinkInsert, new MapSqlParameterSource());
+
         String category = "coffee";
 
         //when
@@ -34,6 +45,7 @@ public class DrinkRepositoryTest {
                 .name("아메리카노")
                 .price(3000)
                 .category(Category.COFFEE)
+                .totalQuantity(4)
                 .build();
         americano.getOptions().add(new Option(1L, "사이즈", "S"));
         americano.getOptions().add(new Option(2L, "사이즈", "M"));
@@ -42,11 +54,12 @@ public class DrinkRepositoryTest {
         americano.getOptions().add(new Option(5L, "온도", "Ice"));
 
         Drink coldbrew = Drink.builder()
-                .id(2L)
-                .img("imgurl2")
-                .name("콜드브루")
-                .price(2000)
+                .id(4L)
+                .img("imgurl4")
+                .name("카페모카")
+                .price(4000)
                 .category(Category.COFFEE)
+                .totalQuantity(3)
                 .build();
 
         Drink drink1 = drinkList.get(0);
